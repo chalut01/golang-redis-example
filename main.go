1f96bb2c71b94a7e81d36bf3f2430a4f8dc5120d
chalut <chalut@techberry.co.th>
2022-04-07 14:06:14 +0700

    hello

--- main.go
+++ main.go
@@ -4,54 +4,67 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
+	"os/exec"
 
 	"github.com/gomodule/redigo/redis"
-	"google.golang.org/appengine"
 )
 
 var redisPool *redis.Pool
 
-func main() {
-	redisAddr := os.Getenv("REDIS_ADDR")
-	redisPassword := os.Getenv("REDIS_PASSWORD")
-
-	redisPool = &redis.Pool{
-		Dial: func() (redis.Conn, error) {
-			conn, err := redis.Dial("tcp", redisAddr)
-			if redisPassword == "" {
-				return conn, err
-			}
-			if err != nil {
-				return nil, err
-			}
-			if _, err := conn.Do("AUTH", redisPassword); err != nil {
-				conn.Close()
-				return nil, err
-			}
-			return conn, nil
-		},
-		// TODO: Tune other settings, like IdleTimeout, MaxActive, MaxIdle, TestOnBorrow.
-	}
-	http.HandleFunc("/", handle)
-	log.Fatal(http.ListenAndServe(":8080", nil))
-	appengine.Main()
-}
-
-func handle(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-
-	redisConn := redisPool.Get()
-	defer redisConn.Close()
-
-	count, err := redisConn.Do("INCR", "count")
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "#Wildfly Exporter for Prometheus V.1.0 by JOM : Path : %s \n \n", r.URL.Path[1:])
+	out, err := exec.Command("hostname").Output()
 	if err != nil {
-		msg := fmt.Sprintf("Could not increment count: %v", err)
-		http.Error(w, msg, http.StatusInternalServerError)
-		return
+		log.Fatal(err)
 	}
-	fmt.Fprintf(w, "Count: %d", count)
+	fmt.Fprintf(w, "%s", out)
 }
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+// func main() {
+// 	redisAddr := os.Getenv("REDIS_ADDR")
+// 	redisPassword := os.Getenv("REDIS_PASSWORD")
+
+// 	redisPool = &redis.Pool{
+// 		Dial: func() (redis.Conn, error) {
+// 			conn, err := redis.Dial("tcp", redisAddr)
+// 			if redisPassword == "" {
+// 				return conn, err
+// 			}
+// 			if err != nil {
+// 				return nil, err
+// 			}
+// 			if _, err := conn.Do("AUTH", redisPassword); err != nil {
+// 				conn.Close()
+// 				return nil, err
+// 			}
+// 			return conn, nil
+// 		},
+// 		// TODO: Tune other settings, like IdleTimeout, MaxActive, MaxIdle, TestOnBorrow.
+// 	}
+// 	http.HandleFunc("/", handle)
+// 	log.Fatal(http.ListenAndServe(":8080", nil))
+// 	appengine.Main()
+// }
+
+// func handle(w http.ResponseWriter, r *http.Request) {
+// 	if r.URL.Path != "/" {
+// 		http.NotFound(w, r)
+// 		return
+// 	}
+
+// 	redisConn := redisPool.Get()
+// 	defer redisConn.Close()
+
+// 	count, err := redisConn.Do("INCR", "count")
+// 	if err != nil {
+// 		msg := fmt.Sprintf("Could not increment count: %v", err)
+// 		http.Error(w, msg, http.StatusInternalServerError)
+// 		return
+// 	}
+// 	fmt.Fprintf(w, "Count: %d", count)
+// }
