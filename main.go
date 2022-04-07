67a342d053b2460bb79a6b8d887655c84acb517f
chalut <chalut@techberry.co.th>
2022-04-07 14:41:03 +0700

    test redis

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"os/exec"
 
 	"github.com/gomodule/redigo/redis"
@@ -12,12 +13,47 @@ import (
 var redisPool *redis.Pool
 
 func handler(w http.ResponseWriter, r *http.Request) {
+
+	redisAddr := os.Getenv("REDIS_ADDR")
+	redisPassword := os.Getenv("REDIS_PASSWORD")
+
+	redisPool = &redis.Pool{
+		Dial: func() (redis.Conn, error) {
+			conn, err := redis.Dial("tcp", redisAddr)
+			if redisPassword == "" {
+				return conn, err
+			}
+			if err != nil {
+				return nil, err
+			}
+			if _, err := conn.Do("AUTH", redisPassword); err != nil {
+				conn.Close()
+				return nil, err
+			}
+			return conn, nil
+		},
+		// TODO: Tune other settings, like IdleTimeout, MaxActive, MaxIdle, TestOnBorrow.
+	}
+
 	fmt.Fprintf(w, "#Wildfly Exporter for Prometheus V.1.0 by JOM : Path : %s \n \n", r.URL.Path[1:])
 	out, err := exec.Command("hostname").Output()
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Fprintf(w, "%s", out)
+	redisConn := redisPool.Get()
+	defer redisConn.Close()
+
+	count, err := redisConn.Do("INCR", "count")
+	if err != nil {
+		msg := fmt.Sprintf("Could not increment count: %v", err)
+		http.Error(w, msg, http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "Count: %d", count)
+}
+func connRedis() {
+
 }
 
 func main() {
